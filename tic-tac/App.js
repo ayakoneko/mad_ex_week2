6497269debb25356a24fb42d1814daf8f41caeb2
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2026-03-06 16:33:37 +1000

    Lab2: Tic Tac Toe board

--- tic-tac/App.js
+++ tic-tac/App.js
@@ -4,7 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style = {styles.row}>
+        <Text style={styles.grid}>O</Text>
+        <Text style={styles.grid}></Text>
+        <Text style={styles.grid}>O</Text>
+      </View>
+      <View style = {styles.row}>
+        <Text style={styles.grid}>X</Text>
+        <Text style={styles.grid}>X</Text>
+        <Text style={styles.grid}>O</Text>
+      </View><View style = {styles.row}>
+        <Text style={styles.grid}>X</Text>
+        <Text style={styles.grid}></Text>
+        <Text style={styles.grid}>O</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c77210e8',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  grid: {
+    width: 80, 
+    height: 80, 
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 40,
+    color: 'white',
+    backgroundColor: '#5a8b3d',
+    borderWidth: 2, 
+    borderColor: '#fff',
   },
 });
